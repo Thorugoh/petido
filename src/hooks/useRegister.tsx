b83f2015fb4f70dec331eea442903f4d541c5422
Victor Hugo <victorhugo@nexti.com>
2021-10-31 20:50:42 -0300

    fix: adiciona metodo correto para persistencia no banco

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -3,6 +3,7 @@ import { Alert } from "react-native";
 import { database, storage } from "../config/firebaseconfig";
 import { Pet, usePetidoContext } from "../context/PetidoContext";
 import uuid from "react-native-uuid";
+import { transformSync } from "@babel/core";
 
 function useRegister() {
   const { loggedUser, setPets } = usePetidoContext();
@@ -21,44 +22,47 @@ function useRegister() {
   }
 
   async function registerPet(pet: Omit<Pet, "id">) {
-    const { situation, colors, size, photo, location } = pet;
-    if (!photo) {
-      Alert.alert(
-        "Foto obrigatória",
-        "Parece que você nao adicionou uma foto."
-      );
-      return;
-    }
+    const { situation, colors, size, location } = pet;
+
     if (!situation || !colors || !size || !location) return;
 
     try {
       const path = `users/${loggedUser.uid}/pets/${uuid.v4()}`;
       const urlImage = await uploadImage(pet.photo, path);
 
-      const userPetsRef = database.ref(`user_pets/${loggedUser.uid}/pets`);
-      await userPetsRef.push({
-        description: pet.description,
-        colors: pet.colors,
-        size: pet.size,
-        location: pet.location,
-        photo: urlImage,
-        situation: pet.situation,
-      });
+      const userPetsRef = database.ref(`/user_pets/${loggedUser.uid}/pets`);
 
-      const petsRef = database.ref("pets");
-      const firebasePets = await petsRef.push({
-        description: pet.description,
-        colors: pet.colors,
-        size: pet.size,
-        location: pet.location,
-        photo: urlImage,
-        situation: pet.situation,
-        user_id: loggedUser.uid,
-      });
+      try {
+        userPetsRef.push({
+          description: pet.description,
+          colors: pet.colors,
+          size: pet.size,
+          location: pet.location,
+          photo: urlImage,
+          situation: pet.situation,
+        });
+      } catch {
+        console.log("error");
+      }
+
+      const petsRef = database.ref("/pets");
+      try {
+        const firebasePets = await petsRef.push({
+          description: pet.description,
+          colors: pet.colors,
+          size: pet.size,
+          location: pet.location,
+          photo: urlImage,
+          situation: pet.situation,
+          user_id: loggedUser.uid,
+        });
+      } catch {
+        console.log("error");
+      }
 
       setPets((pets) => [...pets, { ...pet, id: String(uuid.v4()) }]);
     } catch (err) {
-      console.log(err);
+      console.error("error", err);
     }
   }
 
