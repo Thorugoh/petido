1973c48dca51a199b87b2aad3d68a75bec2a32d8
Victor Hugo <victorhugo@nexti.com>
2021-11-01 23:21:01 -0300

    refactor: remove estilos inline

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/core";
 import React, { useState } from "react";
-import { Alert, View } from "react-native";
+import { Alert, View, StyleSheet } from "react-native";
 import { Button, Text, TextInput, useTheme } from "react-native-paper";
 
 import PetidoeSvg from "../../../resources/petido.svg";
 import { auth } from "../../config/firebaseconfig";
 import { usePetidoContext } from "../../context/PetidoContext";
+import styled from "styled-components/native";
 
 export function SignIn() {
   const { colors } = useTheme();
@@ -57,50 +58,83 @@ export function SignIn() {
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: colors.background,
-      }}
-    >
+    <Container>
       <PetidoeSvg width={200} height={75} />
-      <Text style={{ textAlign: "center", marginTop: 40, fontSize: 20 }}>
-        {`Cadastre-se e faça parte de \numa rede do bem`}
-      </Text>
+      <Title>{`Cadastre-se e faça parte de \numa rede do bem`}</Title>
 
-      <View style={{ height: "40%", width: "80%", alignItems: "center" }}>
-        <TextInput
-          label="Email:"
-          style={{ height: 60, width: "100%" }}
-          onChangeText={setEmail}
-        />
-        <TextInput
+      <InputContainer>
+        <InputEmail label="Email:" onChangeText={setEmail} />
+        <InputPassword
           label="Senha:"
           onChangeText={setPassword}
-          style={{ height: 60, width: "100%", marginTop: 8 }}
           secureTextEntry
         />
-        <TextInput
+        <InputPassword
           label="Repita a Senha:"
           onChangeText={setConfirmPassword}
-          style={{ height: 60, width: "100%", marginTop: 8 }}
           secureTextEntry
         />
-
-        <Button
+        <RegisterButton
           disabled={loading}
           loading={loading}
-          style={{ marginTop: 15, width: "40%" }}
           mode="contained"
-          labelStyle={{ color: "#FFF" }}
+          labelStyle={styles.labelButton}
           onPress={handleSignInWithEmail}
         >
           Cadastrar
-        </Button>
+        </RegisterButton>
         <Button onPress={handleGoToLogin}>Já possuo uma conta, Entrar!</Button>
-      </View>
-    </View>
+      </InputContainer>
+    </Container>
   );
 }
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.Text`
+  text-align: center;
+  margin-top: 40px;
+  font-size: 20px;
+`;
+
+const InputContainer = styled.View`
+  height: 40%;
+  width: 80%;
+  align-items: center;
+`;
+
+const InputEmail = styled(TextInput)`
+  height: 60px;
+  width: 100%;
+`;
+
+const InputPassword = styled(TextInput)`
+  height: 60px;
+  width: 100%;
+  margin-top: 8px;
+`;
+
+const RegisterButton = styled(Button)`
+  margin-top: 25px;
+  width: 60%;
+  color: #fff;
+`;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: { textAlign: "center", marginTop: 40, fontSize: 20 },
+  inputsContainer: { height: "40%", width: "80%", alignItems: "center" },
+  inputEmail: { height: 60, width: "100%" },
+  inputPassword: { height: 60, width: "100%", marginTop: 8 },
+  registerButton: { marginTop: 15, width: "40%" },
+  labelButton: { color: "#FFF" },
+});
