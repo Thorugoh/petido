095dde7c72ea7d9f825c9b7809873cb4c7cf661c
Victor Hugo <victorhugo@nexti.com>
2021-11-20 07:37:12 -0300

    feat: add alert whn rescuing pet

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { StaticMap } from './StaticMap';
 import FoodIcon from "../../resources/pet_food.svg";
@@ -9,115 +9,125 @@ import { useTheme, Button } from 'react-native-paper';
 import { usePetidoContext } from '../context/PetidoContext';
 
 type PetProps = {
-    pet: Pet;
+	pet: Pet;
 }
 
 const sizes = {
-    small: "Pequeno",
-    medium: "Médio",
-    large: "Grande",
+	small: "Pequeno",
+	medium: "Médio",
+	large: "Grande",
 };
 
 const colors = {
-    "1": "1 cor",
-    "2": "2 cores",
-    "3": "3 cores",
+	"1": "1 cor",
+	"2": "2 cores",
+	"3": "3 cores",
 };
 
 const situation = {
-    abandoned: "Abandonado",
-    bruised: "Machucado",
-    lost: "Perdido",
+	abandoned: "Abandonado",
+	bruised: "Machucado",
+	lost: "Perdido",
 };
 
 export function PetCard({ pet }: PetProps) {
-    const { colors: themeColors } = useTheme();
-    const [loading, setLoading] = useState<boolean>(false);
-    const { rescuePet } = usePetidoContext();
+	const { colors: themeColors } = useTheme();
+	const [loading, setLoading] = useState<boolean>(false);
+	const { rescuePet } = usePetidoContext();
 
+	async function confirmRescuePet() {
+		setLoading(true);
+		await rescuePet(pet);
+		setLoading(false);
+	}
+	async function handleRescuePet(pet: Pet) {
+		Alert.alert(
+			"Resgatar Pet",
+			"Você confirma que retirou esse pet da rua?",
+			[
+				{
+					text: "Não",
+					style: "cancel"
+				},
+				{ text: "Sim", onPress: confirmRescuePet }
+			]
+		)
+	}
 
-    async function handleRescuePet(pet: Pet) {
-        setLoading(true);
-        await rescuePet(pet);
-        setLoading(false);
-    }
+	return (
+		<View>
+			<View style={styles.cardContainer}>
+				<View style={{ height: 200 }}>
+					<FastImage
+						style={styles.image}
+						source={{ uri: pet.photo }}
+					/>
+				</View>
+				<StaticMap
+					style={{ width: "100%", height: 100, borderRadius: 10 }}
+					show
+					latitude={pet.location.latitude}
+					longitude={pet.location.longitude}
+				/>
+			</View>
+			<View style={styles.containerDetails}>
+				<View>
+					<View style={{ flexDirection: "row", alignItems: "center" }}>
+						<FoodIcon fill={themeColors.primary} />
+						<Text>{`Porte ${sizes[
+							pet.size
+						].toLowerCase()}`}</Text>
+					</View>
+					<View style={{ flexDirection: "row", alignItems: "center" }}>
+						<MaterialIcons
+							name="pets"
+							color={themeColors.primary}
+							size={17}
+						/>
+						<Text>{`Aparenta estar ${situation[
+							pet.situation
+						].toLowerCase()}`}</Text>
+					</View>
+					<Text style={styles.dogDescription}>{pet.description}</Text>
+				</View>
 
-    return (
-        <View>
-            <View
-                style={{
-                    borderWidth: 1,
-                    marginTop: 15,
-                    borderColor: "#8d8d8d",
-                }}
-            >
-                <View style={{ height: 200 }}>
-                    <FastImage
-                        style={{
-                            width: "100%",
-                            height: "100%",
-                        }}
-                        source={{ uri: pet.photo }}
-                    />
-                </View>
-                <StaticMap
-                    style={{ width: "100%", height: 100, borderRadius: 10 }}
-                    show
-                    latitude={pet.location.latitude}
-                    longitude={pet.location.longitude}
-                />
-            </View>
-            <View
-                style={{
-                    flex: 1,
-                    paddingTop: 6,
-                    marginBottom: 10,
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                }}
-            >
-                <View>
-                    <View style={{ flexDirection: "row", alignItems: "center" }}>
-                        <FoodIcon fill={themeColors.primary} />
-                        <Text style={styles.dogInfo}>{`Porte ${sizes[
-                            pet.size
-                        ].toLowerCase()}`}</Text>
-                    </View>
-                    <View style={{ flexDirection: "row", alignItems: "center" }}>
-                        <MaterialIcons
-                            name="pets"
-                            color={themeColors.primary}
-                            size={17}
-                        />
-                        <Text style={styles.dogInfo}>{`Aparenta estar ${situation[
-                            pet.situation
-                        ].toLowerCase()}`}</Text>
-                    </View>
-                    <Text style={styles.dogDescription}>{pet.description}</Text>
-                </View>
-
-                <Button
-                    disabled={loading}
-                    loading={loading}
-                    mode="contained"
-                    onPress={() => handleRescuePet(pet)}
-                    color={themeColors.secundary}
-                >
-                    Resgatar
+				<Button
+					disabled={loading}
+					loading={loading}
+					mode="contained"
+					onPress={() => handleRescuePet(pet)}
+					color={themeColors.secundary}
+				>
+					Resgatar
             </Button>
-            </View>
-        </View>
-    );
+			</View>
+		</View>
+	);
 }
 
 
 const styles = StyleSheet.create({
-    dogDescription: {
-        fontSize: 14,
-    },
-    dogInfo: {},
-    menuTitles: {
-        fontSize: 18,
-    },
+	dogDescription: {
+		fontSize: 14,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+	},
+	containerDetails: {
+		flex: 1,
+		paddingTop: 6,
+		marginBottom: 10,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	cardContainer: {
+		borderWidth: 1,
+		marginTop: 15,
+		borderColor: "#8d8d8d",
+	},
+	menuTitles: {
+		fontSize: 18,
+	},
 });
